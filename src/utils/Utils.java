8c7671cfb53bda66e434af2ba12d95164798d07b
Jakub Kap <jakub1996k@gmail.com>
2020-05-23 14:29:16 +0200

    Addded conversion byte to int method in Utils

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,9 +1,21 @@
 package utils;
 
+import hla.rti1516e.encoding.DecoderException;
 import hla.rti1516e.encoding.EncoderFactory;
+import hla.rti1516e.encoding.HLAinteger32BE;
+import org.portico.impl.hla1516e.types.encoding.HLA1516eInteger32BE;
 
 public class Utils {
-    public byte[] intToByte(EncoderFactory encoderFactory, int value) {
+    public static byte[] intToByte(EncoderFactory encoderFactory, int value) {
         return encoderFactory.createHLAinteger32BE(value).toByteArray();
+    }
+    public static int byteToInt(byte[] bytes){
+        HLAinteger32BE value = new HLA1516eInteger32BE();
+        try{
+            value.decode(bytes);
+        } catch(DecoderException e){
+            e.printStackTrace();
+        }
+        return value.getValue();
     }
 }
